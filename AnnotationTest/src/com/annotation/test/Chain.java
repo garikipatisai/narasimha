64fc1f71ae33a36763b22d7d442f2f75cc16efbd
garikipatisai <33313962+garikipatisai@users.noreply.github.com>
2017-11-06 08:07:00 -0800

    git
    commited

---
+++ AnnotationTest/src/com/annotation/test/Chain.java
@@ -0,0 +1,22 @@
+package com.annotation.test;
+
+public class Chain {
+private int id;
+private String Type;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getType() {
+	return Type;
+}
+public void setType(String type) {
+	Type = type;
+}
+public String toString() {
+	return "Chain [id=" + id + ", Type=" + Type + "]";
+}
+
+}
---
+++ AnnotationTest/src/com/annotation/test/Motor.java
@@ -0,0 +1,18 @@
+package com.annotation.test;
+
+public class Motor {
+private Chain chain;
+
+public Chain getChain() {
+	return chain;
+}
+
+public void setChain(Chain chain) {
+	this.chain = chain;
+}
+
+public String toString() {
+	return "Motor [chain=" + chain + "]";
+}
+
+}
